5e39451719b5c25bd78ebed0d22802c630389c94
Negan <103877241+NEGAN-579@users.noreply.github.com>
2022-08-22 00:43:38 +0200

    Update Qrcode.py

--- Qrcode.py
+++ Qrcode.py
@@ -8,6 +8,7 @@ import webbrowser
 
 
 
+
 qr = qrcode.QRCode(version=1,
                    error_correction=qrcode.constants.ERROR_CORRECT_L,
                    box_size=10,
@@ -28,31 +29,33 @@ while True:
     qr.make(fit=True)
     qr.add_data(link)
     print('Your link is correct ' + link)
-    Yes_No = input(colored("Yes or NO:\n",'red'))
+    Yes_No = input(colored("Yes or NO:\n",'red')).lower()
     if Yes_No == 'Yes':
         break
     elif Yes_No == 'No':
         continue
 
+def main():
+    global img
+    while True:
+        try:
+            print("\nChoose the color to put on the QR code")
+            color = input()
+            img = qr.make_image(fill_color=color, back_color='white')
+            break
+        except ValueError:
+            print("\nIf you don't know which color to choose go to this link https://en.wikipedia.org/wiki/List_of_colors:_A%E2%80%93F")
+            py.copy('https://en.wikipedia.org/wiki/List_of_colors:_A%E2%80%93F')
+            webbrowser.open("https://en.wikipedia.org/wiki/List_of_colors:_A%E2%80%93F")
+            continue
 
-while True:
-    try:
-        print("\nChoose the color to put on the QR code")
-        color = input()
-        img = qr.make_image(fill_color=color, back_color='white')
-        break
-    except ValueError:
-        print("\nIf you don't know which color to choose go to this link https://en.wikipedia.org/wiki/List_of_colors:_A%E2%80%93F")
-        py.copy('https://en.wikipedia.org/wiki/List_of_colors:_A%E2%80%93F')
-        webbrowser.open("https://en.wikipedia.org/wiki/List_of_colors:_A%E2%80%93F")
-        continue
+    print('What do you want to name the file to save' "\n don't forget to put either .png or .jpg at the end")
+    file = input()
+    print('Your file has been called ' + file)
+    img.save(file)
+    print('\n')
 
-print('What do you want to name the file to save' "\n don't forget to put either .png or .jpg at the end")
-file = input()
-print('Your file has been called ' + file)
-img.save(file)
-print('\n')
-
+main()
 
 
 
